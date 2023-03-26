41b458f6b57306df6f97f365e863c96711ba42d0
Magicoo51889 <jackot11f@gmail.com>
2023-03-26 23:41:45 +0100

    Loading Screen and font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import LoadingScreen from '../components/Loading';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <LoadingScreen />
       <Component {...pageProps} />
       <Analytics />
       <Header />
