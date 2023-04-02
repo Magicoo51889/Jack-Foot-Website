8099f5bc896909014bb954580b02e19b2799804c
Magicoo51889 <jackot11f@gmail.com>
2023-04-02 22:05:07 +0100

    Fixed Card Component
    - Forgot to add routing previously

--- components/Image_Tilt_Card.tsx
+++ components/Image_Tilt_Card.tsx
@@ -1,6 +1,7 @@
 import Tilt from 'react-parallax-tilt';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Link from '../components/NoScrollLink';
 
 const CardTiltAngle:number = 20;
 const CardPerspective:number = 2500;
@@ -8,19 +9,23 @@ const CardPerspective:number = 2500;
 interface TiltCardProps {
     image: string;
     alt: string;
+    link: string;
     w: number;
     h: number;
 }
 
 const TiltCard = (props:TiltCardProps): JSX.Element => {
-    const {image, alt, w, h} = props;
+    const {image, alt, link, w, h} = props;
     return(
         <Tilt tiltMaxAngleX={CardTiltAngle} perspective={CardPerspective} gyroscope={true} tiltReverse={true}>
-            <p className={styles.card}>
-                <span className={styles.logoInvert}>
-                    <Image src={image} alt={alt} width={w} height={h} placeholder='blur' blurDataURL='data:...'/>
-                </span>
-            </p>
+            <Link href={link} >
+                <p className={styles.card}>
+                    <span className={styles.logoInvert}>
+                        <Image src={image} alt={alt} width={w} height={h} placeholder='blur' blurDataURL='data:...'/>
+                    </span>
+                </p>
+            </Link>
+                
         </Tilt>
     )
 }
